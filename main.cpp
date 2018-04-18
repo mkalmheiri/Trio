821c90c19bc26192f77758e9b1a8cddef1da4718
mkalmheiri <mkalmheiri@mail.csuchico.edu>
2018-04-17 22:04:55 -0700

    Trio

--- main.cpp
+++ main.cpp
@@ -1,21 +1,50 @@
 #include <iostream>
 using namespace std;
 
+void greatestToLeast(int&, int&, int&);
+
 int main()
 {
-  //DO NOT CHANGE WITHIN THIS AREA...
-  int red, blue, green;
-  cout<<"Enter Red, Green, and Blue values: ";
-  cin>>red>>green>>blue;
-  //...END OF "DO NOT CHANGE" AREA
+        //DO NOT CHANGE WITHIN THIS AREA...
+        int red, blue, green;
+        cout<<"Enter Red, Green, and Blue values: ";
+        cin>>red>>green>>blue;
+        //...END OF "DO NOT CHANGE" AREA
 
 
+        greatestToLeast(red, green, blue);
 
 
-
-  //DO NOT CHANGE WITHIN THIS AREA...
-  cout<<"Rearranged....\n";
-  cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
-  return 0;
-  //...END OF "DO NOT CHANGE" AREA
+        //DO NOT CHANGE WITHIN THIS AREA...
+        cout<<"Rearranged....\n";
+        cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
+        return 0;
+        //...END OF "DO NOT CHANGE" AREA
 }
+
+void greatestToLeast(int& greatest, int& middle, int& least)
+{ 
+        int number;
+        while(greatest < middle || middle < least || greatest < least)
+        {
+                if(middle > greatest)
+                {
+                        number = greatest;
+                        greatest = middle;
+                        middle = number;
+                }
+                if(least > middle)
+                {
+                        number = middle;
+                        middle = least;
+                        least = number;
+                }
+                if(least > greatest)
+                {
+                        number = greatest;
+                        greatest = least;
+                        least = number;
+                }
+        }
+}
+
